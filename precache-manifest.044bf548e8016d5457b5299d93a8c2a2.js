4bb3799723453d960ec4656478cb519972b5bb9d
Jonas Reinwald <jonas.reinwald@protonmail.com>
2020-04-02 00:04:23 +0200

    Updates

---
+++ precache-manifest.044bf548e8016d5457b5299d93a8c2a2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "037c3f5609dfc6133652be38e6581cc9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c3cc7978b76b35e54c60",
+    "url": "/static/js/2.2da74e91.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.2da74e91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66a05cbf5b18bc138fd9",
+    "url": "/static/js/main.5a003382.chunk.js"
+  },
+  {
+    "revision": "912d01d4d293e1c52c34",
+    "url": "/static/js/runtime-main.70b08fbd.js"
+  }
+]);
