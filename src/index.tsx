16424dd8c80aa18b0bf09e8b54f55e4da112cbd2
Jonas Reinwald <jonas.reinwald@protonmail.com>
2020-04-02 00:57:59 +0200

    add some more elements for fun

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,18 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { Reset } from 'styled-reset'
+import { createGlobalStyle } from 'styled-components'
 
+const GlobalStyle = createGlobalStyle`
+  html, * {
+    font-family: 'Roboto', sans-serif;
+    box-sizing: border-box;
+  }
+`
 ReactDOM.render(
     <React.StrictMode>
         <Reset />
+        <GlobalStyle />
         <App />
     </React.StrictMode>,
     document.getElementById('root')
