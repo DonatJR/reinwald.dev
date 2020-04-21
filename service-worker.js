d61cb5ded7796423c02b118d98f664c23b7aa1f7
Jonas Reinwald <jonas.reinwald@protonmail.com>
2020-04-21 21:53:56 +0200

    update page

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.531e2f36f3349ce012fe6f94630086c6.js"
+  "/precache-manifest.b3266c80aaaa6223709f4ac1e20e689b.js"
 );
 
 self.addEventListener('message', (event) => {
